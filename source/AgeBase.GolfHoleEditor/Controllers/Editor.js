0b8b6bbdd60e32eacd09d90f10c356faff69c1cf
Dan Lister <d.lister@agebase.co.uk>
2014-08-15 22:39:02 +0100

    Added pre value fields for tees, holes and pars.

--- source/AgeBase.GolfHoleEditor/Controllers/Editor.js
+++ source/AgeBase.GolfHoleEditor/Controllers/Editor.js
@@ -1,11 +1,23 @@
 ﻿angular.module("umbraco").controller("AgeBase.GolfHoleEditor", function ($scope) {
 
-    $scope.holes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
-    $scope.pars = [3, 4, 5];
-    $scope.tees = ["Black", "Blue", "White", "Yellow", "Red", "Green"];
+    if ($scope.model.config.tees)
+        $scope.tees = $scope.model.config.tees.split(",");
+    else
+        $scope.tees = ["White", "Yellow", "Red"];
 
-    $scope.init = function() {
+    if ($scope.model.config.holes)
+        $scope.holes = $scope.model.config.holes.split(",");
+    else
+        $scope.holes = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18"];
+
+    if ($scope.model.config.pars)
+        $scope.pars = $scope.model.config.pars.split(",");
+    else
+        $scope.pars = ["3", "4", "5"];
+
+    $scope.init = function () {
         var retval = [];
+
         for (var i = 0; i < $scope.tees.length; i++) {
             retval.push({
                 name: $scope.tees[i],
@@ -14,6 +26,7 @@
                 distance: null
             });
         }
+
         return retval;
     }
 
--- source/AgeBase.GolfHoleEditor/GolfHoleEditorPreValueEditor.cs
+++ source/AgeBase.GolfHoleEditor/GolfHoleEditorPreValueEditor.cs
@@ -6,6 +6,29 @@ namespace AgeBase.GolfHoleEditor
     {
         public GolfHoleEditorPreValueEditor()
         {
+            Fields.Add(new PreValueField
+            {
+                Description = "Enter a comma separated list of tees available for each hole. For example: White,Yellow,Red. Default set to White, Yellow and Red.",
+                Key = "tees",
+                View = "requiredfield",
+                Name = "Tees"
+            });
+
+            Fields.Add(new PreValueField
+            {
+                Description = "Enter a comma separated list of tees available for each hole. For example: 1,2,3,4,5,6,7,8,9. Default set to 1 to 18 holes.",
+                Key = "holes",
+                View = "requiredfield",
+                Name = "Holes"
+            });
+
+            Fields.Add(new PreValueField
+            {
+                Description = "Enter a comma separated list of pars available for each hole. For example: 3,4,5. Default set to pars 3, 4 and 5.",
+                Key = "pars",
+                View = "requiredfield",
+                Name = "Pars"
+            });
         }
     }
 }
