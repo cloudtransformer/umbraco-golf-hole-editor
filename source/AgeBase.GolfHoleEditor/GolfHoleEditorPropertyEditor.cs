2ae1c0938fe1ab653b335c8f2b0eac4b0225c9c7
Dan Lister <d.lister@agebase.co.uk>
2015-02-12 10:34:11 +0000

    Moved embedded resources to app_plugins folder on package.

--- source/AgeBase.GolfHoleEditor/GolfHoleEditorPropertyEditor.cs
+++ source/AgeBase.GolfHoleEditor/GolfHoleEditorPropertyEditor.cs
@@ -4,9 +4,9 @@ using Umbraco.Web.PropertyEditors;
 
 namespace AgeBase.GolfHoleEditor
 {
-    [PropertyEditor("AgeBase.GolfHoleEditor", "AgeBase: Golf Hole Editor", "/App_Plugins/AgeBase.GolfHoleEditor/Resource/Editor.html", ValueType = "TEXT")]
-    [PropertyEditorAsset(ClientDependencyType.Javascript, "/App_Plugins/AgeBase.GolfHoleEditor/Resource/Editor.js")]
-    [PropertyEditorAsset(ClientDependencyType.Css, "/App_Plugins/AgeBase.GolfHoleEditor/Resource/Editor.css")]
+    [PropertyEditor("AgeBase.GolfHoleEditor", "AgeBase: Golf Hole Editor", "/App_Plugins/AgeBase.GolfHoleEditor/Editor.html", ValueType = "TEXT")]
+    [PropertyEditorAsset(ClientDependencyType.Javascript, "/App_Plugins/AgeBase.GolfHoleEditor/Editor.js")]
+    [PropertyEditorAsset(ClientDependencyType.Css, "/App_Plugins/AgeBase.GolfHoleEditor/Editor.css")]
     public class GolfHoleEditorPropertyEditor : PropertyEditor
     {
         protected override PreValueEditor CreatePreValueEditor()
